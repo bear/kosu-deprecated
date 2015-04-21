7c6a57dcf89c0c8133a4b990684f43db25f08d6e
bear (Mike Taylor) <bear@andyet.net>
2015-04-21 15:39:31 -0400

    added rieman example configs

---
+++ riemann/conf.d/tools.clj
@@ -0,0 +1,37 @@
+; straight up log the event
+(defn log-info [event]
+  (info event)
+)
+
+; generate an email from an event
+(def email
+  (mailer {:from "opsbot@example.com"})
+)
+
+(def slack-credentials {
+  :account "", :token ""})
+(def chat-ops
+  (slack slack-credentials {
+    :username "opsbot"
+    :channel "#chatops"
+    :icon ":smile:"}
+))
+
+(def alert-ops
+   (email "opsalert@example.com")
+   (fn [event] (info "alert ops" event))
+)
+
+(def alert-ops-throttled
+  ; throttle to 1000 events every hour
+  (throttle 1000 3600
+    ; of the 1000 throttled above, allow 5 every hour
+    (rollup 5 3600
+      alert-ops
+    )
+  )
+)
+
+(def tell-ops
+   (chat-ops event)
+)
